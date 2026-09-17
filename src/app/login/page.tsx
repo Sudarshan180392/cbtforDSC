7b0b511a7237d9c151c2d76be5b7921a9cd437ad
Sudarshan Mishra <sudarshan03in@gmail.com>
2026-09-17 07:23:19 +0530

    Added Test A/C Details

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -75,7 +75,7 @@ export default function LoginPage() {
           </button>
         </form>
         <div className="mt-4 text-sm text-gray-600 text-center">
-          Test Account: <br /> Roll No: <b>SSC20260001</b> <br /> Password: <b>password123</b>
+          Test Account: <br /> Roll No: <b>DSC003</b> <br /> Password: <b>12345</b>
         </div>
       </div>
     </div>
